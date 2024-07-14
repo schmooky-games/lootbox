1b80576ca6ec4cf01dc207c9bde248aeb8569d10
kaveringleb <kaveringleb@gmail.com>
2024-07-14 14:58:24 +0300

    fix: aioredis connection

--- src/redis_connection.py
+++ src/redis_connection.py
@@ -2,8 +2,4 @@ from redis.asyncio import Redis
 from src.config import REDIS_URI
 
 
-def create_redis_connection():
-    return Redis.from_url(REDIS_URI, encoding="utf-8", decode_responses=True)
-
-
-redis = create_redis_connection()
+redis = Redis.from_url(REDIS_URI, encoding="utf-8", decode_responses=True)
