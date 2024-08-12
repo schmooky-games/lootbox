42fb178b4d0ced015372a29373975fc145c60386
kaveringleb <kaveringleb@gmail.com>
2024-08-12 23:26:37 +0300

    fix: use uvloop

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-# import uvloop
-# uvloop.install()
+import uvloop
+uvloop.install()
 from fastapi import FastAPI
 from fastapi_healthcheck import HealthCheckFactory, healthCheckRoute
 from prometheus_fastapi_instrumentator import Instrumentator
